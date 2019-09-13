a896c521c81b0c1263e234002676c949d73ae461
oliv008b <55293816+oliv008b@users.noreply.github.com>
2019-09-13 21:41:23 +0200

    Delete welcome.js

--- welcome.js
+++
@@ -1,6 +0,0 @@
-bot.on('guildMemberAdd', member => {
-    member.guild.channels.get('527549468824829995').send("Hej " + member.user +", Velkommen til **MERATH - NPRISON** :tada::hugging:! - Håber du nyder dit ophold på MERATH  :rainbow: :blush:\n\n ***HUSK!*** At læse vores Regler ind under #📋regler📋 - Tak på forhånd!:stuck_out_tongue_winking_eye:\n\nKunne du tænke dig at donere til serveren? - /buy:wink:✌️\n\nSer du en Hacker?! Report spilleren ind under #💣report-en-hacker💣  - Så vil STAFF TEAM klar resten! Tak skal du have! :skull::robot:\n\n"); 
-
-});
-
-    
